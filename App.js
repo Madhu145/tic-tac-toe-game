4f990209a6c28e63e19ceba23e17af4444839ed1
Madhu145 <sakemadhu1436@gmail.com>
2024-07-12 11:45:43 +0530

    project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React1 from './registration';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <React1 />
+      </header>
+    </div>
+  
+  );
+}
+export default App;
---
+++ registration.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '.App.css';
+function React1() {
+  return (
+    <div>
+
+        <p>
+          Registration form 
+        </p>
+    </div>
+  );
+}
+
+export default React1;
+
